9dd00ea0466ddf09ff6f96a088143d76a3a15c68
Fang <felord@qq.com>
2022-04-16 14:49:36 +0800

    Initial commit

---
+++ src/main/java/cn/felord/idserver/IdServerApplication.java
@@ -0,0 +1,13 @@
+package cn.felord.idserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdServerApplication.class, args);
+    }
+
+}
