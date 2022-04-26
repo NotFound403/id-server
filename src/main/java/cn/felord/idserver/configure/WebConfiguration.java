2f4e618cb4cb48b4f1f24a500468b18d870f4b65
xiaoshitou <stone981023@gmail.com>
2022-04-26 15:49:50 +0800

    [新增功能](ui): 修改序列化配置

--- src/main/java/cn/felord/idserver/configure/WebConfiguration.java
+++ src/main/java/cn/felord/idserver/configure/WebConfiguration.java
@@ -1,19 +1,21 @@
 package cn.felord.idserver.configure;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Web Configuration
+ *
+ * @author zhengchalei
+ */
 @Configuration
 public class WebConfiguration {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .registerModule(new Hibernate5Module());
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.modulesToInstall(new Hibernate5Module());
     }
 
 }
